d75bdc5f5da206f5ad9a845015205ee1714eee26
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-20 20:31:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,8 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "gmlEQQgK#I3p8_RkDchmZYAjNsuKWATOwQIJjyJW6C_ReK7-4oMY",
 MONGODB: process.env.MONGODB || "mongodb://mongo:cRkswBWkzOcbjKHaQfDsLLraLympnLQa@junction.proxy.rlwy.net:56096",
 PEXELS_API_KEY: process.env.PEXELS_API_KEY='39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH',
+AUTO_TYPING: process.env.AUTO_TYPING === undefined ? 'false' : process.env.AUTO_TYPING,
+AUTO_RECORDING: process.env.AUTO_RECORDING === undefined ? 'true' : process.env.AUTO_RECORDING,
+STATUS_VIEW: process.env.STATUS_VIEW === undefined ? 'true' : process.env.STATUS_VIEW,
+ALWAYS_ONLINE: process.env.ADDRESSES === undefined ? 'false' : process.env.ADDRESSES,
 };
