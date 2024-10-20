3d1a78bc985e5d04c5959da5ddecae0194d04001
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-20 22:14:00 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,7 +10,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const config = await readEnv();
-letmenu = {
+let menu = {
 main: '',
 download: '',
 news: '',
