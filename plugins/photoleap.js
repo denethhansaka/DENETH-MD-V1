f91f2a6b6969e2b57cc9961ef439ab924983361e
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-20 17:36:46 +0530

    Update photoleap.js

--- plugins/photoleap.js
+++ plugins/photoleap.js
@@ -37,7 +37,7 @@ try {
     if (!q) return reply('Please provide a name');
     await conn.sendMessage(from, { 
         image: { url: `https://www.flamingtext.com/net-fu/proxy_form.cgi?&script=fluffy-logo&text=${q}` }, 
-        caption: '> 🇵​​🇴​​🇼​​🇪​​🇷​​🇪​​🇩​ ​🇧​​🇾​ ​🇩​​🇪​​🇳​​🇪​​🇹​​🇭​-​🇲​​🇩​ ​🇼​​🇭​​🇦​​🇹​​🇸​​🇦​​🇵​​🇵​ ​🇧​​🇴​​🇹​®` 
+        caption: '> 🇵​​🇴​​🇼​​🇪​​🇷​​🇪​​🇩​ ​🇧​​🇾​ ​🇩​​🇪​​🇳​​🇪​​🇹​​🇭​-​🇲​​🇩​ ​🇼​​🇭​​🇦​​🇹​​🇸​​🇦​​🇵​​🇵​ ​🇧​​🇴​​🇹​®' 
     }, {quoted: mek});
 
 } catch (e) {
